bbae5ed2969c610e334b3585bef105bc196e654f
simon198 <c121549443@gmail.com>
2020-12-03 15:46:10 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3001;
+
+app.set("trust proxy", 1);
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  })
+);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`「 學習平台啟動！」 Listening on http://localhost:${port}`);
+});
